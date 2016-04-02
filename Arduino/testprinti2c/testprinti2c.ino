43358912dda41bb830f8e4ed5497aa0056e09006
Oleg Vaskevich <oleg@osv.im>
2016-04-02 15:43:54 -0400

    Add I2C test Arduino code

---
+++ Arduino/testprinti2c/testprinti2c.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+
+int sensorPin = A3;
+int sensorValue = 0;
+
+void setup() {
+  Wire.begin(0x77);
+  Serial.begin(9600);
+  Wire.onReceive(printReceived);
+  Serial.println(F("Ready!"));
+}
+
+void printReceived(int numBytes) {
+  Serial.println(F("Received data from I2C!"));
+  while (Wire.available()) {
+    unsigned int num = (unsigned int) Wire.read();
+    Serial.println(String(num));
+  }
+}
+
+void loop() {
+}
+
