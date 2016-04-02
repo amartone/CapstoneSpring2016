6a59f6445ad9650f977dee8928dd2d089ee88550
Oleg Vaskevich <oleg@osv.im>
2016-04-02 19:01:48 -0400

    Fix up code for automated BP Arduino

--- Arduino/automated_bp/automated_bp.ino
+++ Arduino/automated_bp/automated_bp.ino
@@ -1,133 +1,149 @@
 #include <AFMotor.h>
+#include <Wire.h>
 
 AF_DCMotor motor(1);
 
-const int motorPin = 5;
-const int solValve = 7;
-const int transducer = A0;
+const int kMotorPin = 5;
+const int kSolValve = 7;
+const int kTransducer = A0;
 const int kMaxPressure = 634;
+const int kMotorSpeed = 200;
+const int kI2cSlaveAddress = 0x77;
 
+bool has_finished = false;
+
+void TransmitPressureViaI2C(int diastole, int systole) {
+  // TODO
+}
 
 void setup() {
+  // Set up the serial connection.
   Serial.begin(9600);
+
+  // Set up the I2C connection.
+  Wire.begin(kI2cSlaveAddress);
+  Wire.onReceive(printReceived);
 
   // Configure the solenoid valve.
   pinMode(solValve, OUTPUT);
 
-  // Configure and turn on the motor.
-  motor.setSpeed(200);
-  motor.run(RELEASE);
+  // Configure the motor.
+  motor.setSpeed(kMotorSpeeed);
 }
 
-void loop() {  
-  if (END == 0) {
-    delay(2000);
-    // Open the solenoid valve
-    analogWrite(solValve, 674);  // 674 == 3.3V
+void loop() {
+  if (has_finished) {
+    return;
+  }
 
-    // Start the pump.
-    int speed = 255;  // Is this the max speed of the pump?
-    //  analogWrite(motorPin, speed);
+  // Start the motor and wait a couple seconds for the pump to inflate.
+  motor.run(RELEASE);
+  delay(2000);
 
-    // Read Pressure
-    int pressure = analogRead(transducer);
-    // Continously read the pressure until we hit 634.
-    //  motor.run(FORWARD);
-    while (pressure < kMaxPressure) {
-      pressure = analogRead(transducer);
-      Serial.println(pressure);
-      delay(55);
-    }
+  // Open the solenoid valve.
+  analogWrite(kSolValve, 674);  // 674 == 3.3V
 
-    // Shut off the motor.
-    speed = 0;
-    //  analogWrite(motorPin, speed);
-    motor.setSpeed(speed);
-    delay(5000);
+  // Continously read the pressure until we hit the pressure shut-off value.
+  int pressure = analogRead(transducer);
+  while (pressure < kMaxPressure) {
+    pressure = analogRead(transducer);
+    Serial.println(pressure);
+    delay(55);
+  }
 
-    int x[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int time[] = {300, 300, 300, 300, 300, 300, 300, 300,
-                  400, 400, 400, 400, 400, 400, 400};
-    int y[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int p1;
-    int p2;
-    int diastole = 1024;
-    int systole = 0;
-    int pressureReadings[100];
+  // Shut off the motor and wait a few seconds to restrict blood flow.
+  motor.run(BREAK);
+  delay(5000);
 
-    // Turn the valve on and off overtime
-    for (int j = 0; j < 15; j++) {
-      analogWrite(solValve, 0);  // shut the valve off
-      delay(100);
-      analogWrite(solValve, 674);  // turn the valve on
-      delay(250);
-      // Read pressure values
-      for (int i = 0; i < 100; i++) {
-        pressureReadings[i] = analogRead(transducer);
-        delay(10);
-        Serial.println(pressureReadings[i]);
+  int x[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int time[] = {300, 300, 300, 300, 300, 300, 300, 300,
+                400, 400, 400, 400, 400, 400, 400};
+  int y[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int p1;
+  int p2;
+  int diastole = 1024;
+  int systole = 0;
+  int pressureReadings[100];
 
-        // Check for heartbeats
-        // If the previous values are greater than current values
-        if ((pressureReadings[i] - 3) > pressureReadings[i - 1]) {
-          Serial.println("Hearbeat detected");
-          Serial.println(pressureReadings[i] - 3);
-          if (pressureReadings[i] > systole) {
-            systole = pressureReadings[i];
-            Serial.println("Systole");
-            Serial.println(systole);
-          }
-          if (pressureReadings[i] < diastole) {
-            diastole = pressureReadings[i];
-            Serial.println("Diastole");
-            Serial.println(diastole);
-          }
+  // Turn the valve on and off over time.
+  for (int j = 0; j < 15; j++) {
+    // Shut the valve off.
+    analogWrite(solValve, 0);
+    delay(100);
+    analogWrite(solValve, 674);
+
+    // Turn the valve on.
+    delay(250);
+
+    // Read pressure values.
+    for (int i = 0; i < 100; i++) {
+      pressureReadings[i] = analogRead(transducer);
+      delay(10);
+      Serial.println(pressureReadings[i]);
+
+      // Check for heartbeats, which can be detected if the previous
+      // values are greater than the current values.
+      if ((pressureReadings[i] - 3) > pressureReadings[i - 1]) {
+        Serial.println("Heartbeat detected.");
+        Serial.println(pressureReadings[i] - 3);
+        if (pressureReadings[i] > systole) {
+          systole = pressureReadings[i];
+          Serial.println("Updated systolic pressure:");
+          Serial.println(systole);
+        }
+        if (pressureReadings[i] < diastole) {
+          diastole = pressureReadings[i];
+          Serial.println("Updated diastolic pressure:");
+          Serial.println(diastole);
         }
       }
-      // delay(200);
-      // y[j] = max(p1, p2) - pressure;
-
-      Serial.println("Diastole");
-      Serial.println(diastole);
-      Serial.println("Systole");
-      Serial.println(systole);
     }
+    // delay(200);
+    // y[j] = max(p1, p2) - pressure;
 
-    //   int maxm = 0;
-    //   int i;
-    //
-    //   for(i=5; i<15; i++){
-    //     if(y[i]>maxm){
-    //       maxm = y[i];
-    //     }
-    //   }
-    //
-    //   int index[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    //   int k;
-    //   int l=0;
-    //
-    //   for (k=5; k<15; k++){
-    //     if(y[k]==maxm){
-    //       index[k-5] = k;
-    //       l++;
-    //     }
-    //
-    //   }
-    //
-    // int total = 0;
-    // int a;
-    // for(a=0; a<1; a++){
-    //   total = x[index[a]] + total;
-    //
-    // }
-    //
-    // int MAP = ((total / l)-550)*200/102;
-    // Serial.println("Mean Arterial pressure");
-    // Serial.print (MAP);
-    // Serial.print ("mmHg");
+    Serial.println("Final diastolic pressure:");
+    Serial.println(diastole);
+    Serial.println("Final systolic pressure:");
+    Serial.println(systole);
 
-    analogWrite(solValve, 0);
-    motor.run(RELEASE);
-    END++;
+    TransmitPressureViaI2C(diastole, systole);
   }
+
+  // Release the solenoid valve.
+  analogWrite(solValve, 0);
+  has_finished = true;
 }
+
+// // Mean Arterial pressure code?
+//   int maxm = 0;
+//   int i;
+//
+//   for(i=5; i<15; i++){
+//     if(y[i]>maxm){
+//       maxm = y[i];
+//     }
+//   }
+//
+//   int index[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+//   int k;
+//   int l=0;
+//
+//   for (k=5; k<15; k++){
+//     if(y[k]==maxm){
+//       index[k-5] = k;
+//       l++;
+//     }
+//
+//   }
+//
+// int total = 0;
+// int a;
+// for(a=0; a<1; a++){
+//   total = x[index[a]] + total;
+//
+// }
+//
+// int MAP = ((total / l)-550)*200/102;
+// Serial.println("Mean Arterial pressure");
+// Serial.print (MAP);
+// Serial.print ("mmHg");
