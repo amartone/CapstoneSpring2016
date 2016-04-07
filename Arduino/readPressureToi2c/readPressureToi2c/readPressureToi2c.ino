2d1650ef83d435655a635792d4d1c0b184b1bc33
Oleg Vaskevich <oleg@osv.im>
2016-04-07 17:28:27 -0400

    Clean up and remove unused code from readPressutToI2c.

--- Arduino/readPressureToi2c/readPressureToi2c/readPressureToi2c.ino
+++ Arduino/readPressureToi2c/readPressureToi2c/readPressureToi2c.ino
@@ -2,47 +2,21 @@
 
 const int kTransducer = A0;
 
-const int kMaxPressure = 634;
-
 const int kI2cSlaveAddress = 0x77;
 const char kResponsePressureNotYetAvailable[] = {(char) 0x23, (char) 0x23, (char) 0x23};
 const char kResponsePressureAvailable = (char) 0x24;
-const int kReadPressureCommand = 0x42;
-
-unsigned int pressureValue;
-bool measure_requested = false;
-bool pressure_measured = false;
-
-void CommandReceived(int numBytes) {
-  //Serial.println(F("Received command from ADUCM350!"));
-  while (Wire.available()) {
-    unsigned int num = (unsigned int) Wire.read();
-    if (num == kReadPressureCommand) {
-      //Serial.println(F("ADUCM350 requested cuff pressure."));
-      measure_requested = true;
-    }
-  }
-  //Serial.println("CommandReceived");
-}
-
-
 
 void RequestReceived() {
-  //if (!pressure_measured) {
-  //  Wire.write(kResponsePressureNotYetAvailable, 3);
-  //} else {
-    char to_write[3];
-    to_write[0] = kResponsePressureAvailable;
-    to_write[1] = (char) analogRead(kTransducer);
-    to_write[2] = (char) (analogRead(kTransducer) >> 8);
+  // Read from the transducer; this is the only value that the master needs.
+  int val = analogRead(kTransducer);
 
-    Wire.write(to_write, 3);
-  //}
-  //Serial.println("RequestReceived");
-  //Serial.println(analogRead(kTransducer));
+  unsigned char to_write[3];
+  to_write[0] = kResponsePressureAvailable;
+  to_write[1] = (unsigned char) val;
+  to_write[2] = (unsigned char) (val >> 8);
+
+  Wire.write(to_write, 3);
 }
-
-
 
 void setup() {
   // Set up the serial connection.
@@ -50,17 +24,9 @@ void setup() {
 
   // Set up the I2C connection.
   Wire.begin(kI2cSlaveAddress);
-  Wire.onReceive(CommandReceived);
-  Wire.onRequest(RequestReceived);
-  
+  Wire.onRequest(RequestReceived);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-   if (!measure_requested) {
-    return;
-  }
-  //Serial.println("measure requested");
-  
-
+  // Unused.
 }
