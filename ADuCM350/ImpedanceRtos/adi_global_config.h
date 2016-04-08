f846f447eecc952734a9462c999b89ad1c6edac9
Oleg Vaskevich <oleg@osv.im>
2016-04-07 20:41:34 -0400

    Add ImpedanceRtos code

---
+++ ADuCM350/ImpedanceRtos/adi_global_config.h
@@ -0,0 +1,8 @@
+/* This file contains all the global configurations */
+#ifndef __ADI_GLOBAL_CONFIG_H__
+#define __ADI_GLOBAL_CONFIG_H__
+
+/*! Set this macro to 1 to enable multi threaded support */
+#define ADI_CFG_ENABLE_RTOS_SUPPORT                       1
+
+#endif /* __ADI_GLOBAL_CONFIG_H__ */
