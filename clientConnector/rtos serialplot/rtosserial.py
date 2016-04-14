6558976b7c17c7f187685a40f1c7801f4a23afdf
ksmizzle <smith.kenny11@gmail.com>
2016-04-14 04:08:48 -0400

    update rtosserial

--- clientConnector/rtos serialplot/rtosserial.py
+++ clientConnector/rtos serialplot/rtosserial.py
@@ -13,7 +13,7 @@ import requests
 def datacollection(ser):
     jsondata = {}
     
-    jsondata["userId"]="23423423423423432"
+    jsondata["userId"]="57047c575ce7f99a3d258a7b"
     jsondata["measurements"] = []
     while(True):
         rawinput = ser.readline()
@@ -25,25 +25,31 @@ def datacollection(ser):
             jsondata["measurements"].append({"impedance_magnitude":data[1], "impedance_phase":data[2], "pressure":data[0]})
             
 def plotdata(data):
-
-    numvals = length(data["measurements"])
+    data["measurements"].pop(0)
+    data["measurements"].pop(0)
+    data["measurements"].pop(0)
+    numvalues = len(data["measurements"])
+    pressure = [x["pressure"]for x in data["measurements"]]
+    magnitude = [x["impedance_magnitude"]for x in data["measurements"]]
+    phase = [x["impedance_phase"]for x in data["measurements"]]
+    # print(str(pressure))
     x = np.arange(0, numvalues, 1)
+    
   
   
     plt.subplot(2, 1, 1)
-    plt.plot(mag)
+    plt.plot(magnitude)
     plt.title('Pressure and Impedance over Time')
     plt.ylabel('Impedance Magnitude')
     plt.autoscale(enable=True, axis = 'both', tight = None)
     plt.subplot(2, 1, 2)
-    plt.plot(pres)
+    plt.plot(pressure)
     plt.ylabel('Pressure')
     plt.xlabel('sample')
     plt.autoscale(enable=True, axis = 'both', tight = None)
     plt.show()
 
 def main():
-  numvalues = 10000
 
 
   # create parser
@@ -61,17 +67,19 @@ def main():
   ser.baudrate = 115200
   ser.port = 'COM8'
   ser.open()
+  ser.flush()
   while(True):
     input = ser.readline()
     print(str(input))
     if(b'START' in input):
         print("starting data collection")
         jsondata = datacollection(ser)
+        plotdata(jsondata)
         # print(str(bpsamples))
         
-        url = "http://capstone-martoneandrew.rhcloud.com/api/capstone/bp/"
-        r = requests.post(url, json=jsondata)
-        print("status from post: " +str(r.status_code))
+        # url = "http://capstone-martoneandrew.rhcloud.com/api/capstone/bp/"
+        # r = requests.post(url, json=jsondata)
+        # print("status from post: " +str(r.status_code))
         break
         
         
@@ -87,7 +95,7 @@ def main():
   # jsondata["measurements"] = []
   # pressure = np.empty([numvalues])
   # magnitude = np.empty([numvalues])
-  print("[")
+  # print("[")
   # for i in range(numvalues):
     # print("on number " +str(i))
     # serline = ser.readline()
@@ -99,9 +107,9 @@ def main():
         # jsondata["measurements"].append({"impedance_magnitude":data[1], "impedance_phase":data[2], "pressure":data[0]})
 
   
-  url = "http://capstone-martoneandrew.rhcloud.com/api/capstone/bp/"
-  r = requests.post(url, json=jsondata)
-  print("status from post: " +str(r.status_code))
+  # url = "http://capstone-martoneandrew.rhcloud.com/api/capstone/bp/"
+  # r = requests.post(url, json=jsondata)
+  # print("status from post: " +str(r.status_code))
   
   # x = np.arange(0, numvalues, 1)
   
